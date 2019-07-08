7569e95a5461cf9ccd9347860087d3cb3b1f7c98
thosefacts <thosefactsemail@gmail.com>
2019-07-08 14:52:33 +0200

    first commit

---
+++ MainPages/summaryscript.js
@@ -0,0 +1,11 @@
+const det = Array.from(document.querySelectorAll("details"));
+
+det.forEach((TheDetail) => {
+TheDetail.addEventListener("click", () => {
+det.forEach((det) => {
+    if (det !== TheDetail) {
+    det.removeAttribute("open");
+}
+});
+});
+});
