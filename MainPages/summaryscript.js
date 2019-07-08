926218e2451c5f6c8443b7f3120473de568b25d9
thosefacts <52661095+thosefacts@users.noreply.github.com>
2019-07-08 16:25:54 +0200

    hiding browserbar folder name (aka rewrite url)

--- MainPages/summaryscript.js
+++ MainPages/summaryscript.js
@@ -9,3 +9,11 @@ det.forEach((det) => {
 });
 });
 });
+
+Function GetCategory() As String
+If (Request.PathInfo.Length = 0) Then
+Return ""
+Else
+Return Request.PathInfo.Substring(1)
+End If
+End Function
